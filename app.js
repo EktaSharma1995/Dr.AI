a7fdac4310df9cdb21206adf38e15e0539ff0590
Ekta Sharma <ektasharma@Ektas-MacBook-Air.local>
2016-12-26 10:09:38 +0530

    Profile information

--- app.js
+++ app.js
@@ -5,47 +5,213 @@ var builder = require('botbuilder');
 //=========================================================
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(3000, function () {
-   console.log('%s listening to %s', server.name, server.url);
+server.listen(3000, function() {
+	console.log('%s listening to %s', server.name, server.url);
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "86dfb2c7-fcc1-47f3-a5b3-0d28421e9b4c",
-    appPassword: "OWPdaWzk9paOcnRB19kKq1d"
+	appId : "86dfb2c7-fcc1-47f3-a5b3-0d28421e9b4c",
+	appPassword : "OWPdaWzk9paOcnRB19kKq1d"
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 //Bot on
-bot.on('contactRelationUpdate', function (message) {
-    if (message.action === 'add') {
-        var name = message.user ? message.user.name : null;
-        var reply = new builder.Message()
-                .address(message.address)
-                .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
-        bot.send(reply);
-    } else {
-        // delete their data
-    }
+bot
+		.on(
+				'contactRelationUpdate',
+				function(message) {
+					if (message.action === 'add') {
+						var name = message.user ? message.user.name : null;
+						var reply = new builder.Message()
+								.address(message.address)
+								.text(
+										"Hello %s... Thanks for adding me. Say 'hello' to see some great demos.",
+										name || 'there');
+						bot.send(reply);
+					} else {
+						// delete their data
+					}
+				});
+bot.on('typing', function(message) {
+	// User is typing
 });
-bot.on('typing', function (message) {
-  // User is typing
-});
-bot.on('deleteUserData', function (message) {
-    // User asked to delete their data
+bot.on('deleteUserData', function(message) {
+	// User asked to delete their data
 });
 //=========================================================
 // Bots Dialogs
 //=========================================================
-String.prototype.contains = function(content){
-  return this.indexOf(content) !== -1;
+String.prototype.contains = function(content) {
+	return this.indexOf(content) !== -1;
 }
-bot.dialog('/', function (session) {
-	console.log(session.message.text);
-    if(session.message.text.toLowerCase().contains('hello')){
-      session.send('Hey, How are you?');
-      }else if(session.message.text.toLowerCase().contains('help')){
-        session.send('How can I help you?');
-      }else{
-        session.send('Sorry I do not understand you...');
-      }
-});
+
+bot.dialog('/', 
+		[ 
+		  function(session) {
+			  session.beginDialog('/name');
+		  },
+		  function(session) {
+			  session.beginDialog('/age');
+		  },
+		  function(session) {
+			  session.beginDialog('/gender');
+		  },
+		  function(session) {
+			  session.beginDialog('/symptoms');
+		  }
+		 ]);
+
+bot.dialog('/name', [ 
+         function(session, args, next) {
+        	 builder.Prompts.text(session, "What's your name?");
+         		}, 
+         function(session, results, next) {
+         	 session.dialogData.name = results.response;
+         	 session.endDialog();
+         		 }
+         		]);
+
+bot.dialog('/age', [ 
+                     function(session, args, next) {
+                    	 builder.Prompts.text(session, "What's your age?");
+                     		}, 
+                     function(session, results, next) {
+                     	 session.dialogData.age = results.response;
+                     	 session.endDialog();
+                     		}
+                     ]);
+
+bot.dialog('/gender', [ 
+                     function(session, args, next) {
+                    	 builder.Prompts.text(session, "What's your gender?M/F");
+                     		}, 
+                     function(session, results, next) {
+                     	 session.dialogData.gender = results.response;
+                     	 session.endDialog();
+                     		 }
+                     		]);
+bot.dialog('/symptoms', [ 
+                     function(session, args, next) {
+                    	 builder.Prompts.text(session, "What's your symptoms?");
+                     		}, 
+                     function(session, results, next) {
+                     	 session.dialogData.symptoms = results.response;
+                     	 session.endDialog();
+                     		 }
+                     		]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//bot.dialog('/', [
+//                 function (session, args, next) {
+//                     if (!session.userData.name) {
+//                         session.beginDialog('/name');
+//                     } else {
+//                         next();
+//                     }
+//                 },
+//                 function (session, results) {
+//                     session.send('Hello %s!', session.userData.name);
+//                 }
+//             ]);
+//
+//		    bot.dialog('/name', [
+//			     function (session) {
+//				 builder.Prompts.text(session, 'Hi! What is your name?');
+//				 },
+//				 function (session, results) {
+//				 session.userData.name = results.response;
+//			     session.endDialog();
+//				 }
+//		      ]);
+//		    
+// bot.dialog('/', [
+//                  function (session, args, next) {
+//                      if (!session.userData.age) {
+//                          session.beginDialog('/age');
+//                      } else {
+//                          next();
+//                      }
+//                  },
+//                  function (session, results) {
+//                      session.send('Hello %i!', session.userData.age);
+//                  }
+//              ]);
+//
+//  bot.dialog('/age', [
+//      function (session) {
+//          builder.Prompts.text(session, 'Hi! What is your age?');
+//      },
+//      function (session, results) {
+//          session.userData.age = results.response;
+//          session.endDialog();
+//      }
+//  ]);
+//
+//  bot.dialog('/', [
+//                   function (session, args, next) {
+//                       if (!session.userData.gender) {
+//                           session.beginDialog('/gender');
+//                       } else {
+//                           next();
+//                       }
+//                   },
+//                   function (session, results) {
+//                       session.send('Hello %s!', session.userData.gender);
+//                   }
+//               ]);
+//
+//   bot.dialog('/gender', [
+//       function (session) {
+//           builder.Prompts.text(session, 'Hi! What is your gender?');
+//       },
+//       function (session, results) {
+//           session.userData.gender = results.response;
+//           session.endDialog();
+//       }
+//   ]);  
+//
+//
+
+//bot.dialog('/', [
+//                 function (session) {
+//                	 builder.Prompts.text(session, 'Hi! What is your name?');
+//                 },
+//                 function (session, results) {
+//                	 session.userData.name = results.response;
+//                	 session.endDialog();
+//         	        session.send('Hello %s!', session.userData.name);
+//                 },
+//                 
+//                 bot.dialog('/profile', [
+//                                         function (session) {
+//                                             builder.Prompts.text(session, 'Hi! What is your name?');
+//                                         },
+//                                         function (session, results) {
+//                                             session.userData.name = results.response;
+//                                             session.endDialog();
+//                                         }
+//                                     ]
+//                ]);
+
+//bot.dialog('/', function (session) {
+//	console.log(session.message.text);
+//    if(session.message.text.toLowerCase().contains('hello')){
+//      session.send('Hey, How are you?');
+//      }else if(session.message.text.toLowerCase().contains('help')){
+//        session.send('How can I help you?');
+//      }else{
+//        session.send('Sorry I do not understand you...');
+//      }
+//});
